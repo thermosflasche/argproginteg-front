18b39e55f71eba586a8363334f22e480fb92a65c
DebianMan <>
2023-04-21 00:02:43 -0300

    go back

--- src/app/autorizado.guard.ts
+++ src/app/autorizado.guard.ts
@@ -1,31 +1,30 @@
-import { Injectable, inject } from '@angular/core';
-import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, CanActivateFn, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
-import { AutenticacionService } from './autenticacion.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AutorizadoGuard {
-//  constructor(private ruta:Router){}
-//  canActivate(
-//    route: ActivatedRouteSnapshot,
-//    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-//    if (localStorage.getItem("u")||"".startsWith("Bearer ")){
-//      return true;
-//    } else {
-//      this.ruta.navigate(["/login"]);
-//      return false;
-//    }
-//  }
-  guardia = () => {
-    const ruta = inject(Router);
+export default class AutorizadoGuard implements CanActivate {
+  constructor(private ruta:Router){}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     if (localStorage.getItem("u")||"".startsWith("Bearer ")){
       return true;
     } else {
-      ruta.navigate(["/login"]);
+      this.ruta.navigate(["/login"]);
       return false;
     }
   }
+//  guardia = () => {
+//    const ruta = inject(Router);
+//    if (localStorage.getItem("u")||"".startsWith("Bearer ")){
+//      return true;
+//    } else {
+//      ruta.navigate(["/login"]);
+//      return false;
+//    }
+//  }
   
 }
